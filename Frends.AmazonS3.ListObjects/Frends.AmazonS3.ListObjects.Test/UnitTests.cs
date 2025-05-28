11a7b769d116147b6375b7fdd0fb5bb74dc9dfd6
MichalFrends1 <michal.nowakowski@frends.com>
2025-05-28 10:55:55 +0200

    testing InvalidBucketName_ShouldThrowAmazonS3Exception - added console log

--- Frends.AmazonS3.ListObjects/Frends.AmazonS3.ListObjects.Test/UnitTests.cs
+++ Frends.AmazonS3.ListObjects/Frends.AmazonS3.ListObjects.Test/UnitTests.cs
@@ -184,6 +184,10 @@ namespace Frends.AmazonS3.ListObjects.Test
             };
 
             var ex = Assert.ThrowsExceptionAsync<Exception>(async () => await AmazonS3.ListObjects(invalidSource, _options, default)).Result;
+            
+            Console.WriteLine("Exception Type: " + ex.GetType().FullName);
+            Console.WriteLine("Message: " + ex.Message);
+            Console.WriteLine("StackTrace: " + ex.StackTrace);
 
             Assert.IsTrue(ex.Message.Contains("The specified bucket does not exist"));
         }
