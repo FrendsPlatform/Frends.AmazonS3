8d4f9465c7213b0555fe6c4fb6818f5bd47ea761
Gabriel <gabriel.dearden@frends.com>
2025-06-18 10:38:38 +0300

    Added ThrowErrorOnFailure to Options.cs

--- Frends.AmazonS3.CreateBucket/Frends.AmazonS3.CreateBucket/Definitions/Options.cs
+++ Frends.AmazonS3.CreateBucket/Frends.AmazonS3.CreateBucket/Definitions/Options.cs
@@ -14,5 +14,13 @@ namespace Frends.AmazonS3.CreateBucket.Definitions
         /// <example>false</example>
         [DefaultValue(false)]
         public bool ObjectLockEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether an error should stop the Task and throw an exception.
+        /// If set to true, an exception will be thrown when an error occurs. If set to false, Task will try to continue and the error message will be added into Result.ErrorMessage and Result.Success will be set to false.
+        /// </summary>
+        /// <example>true</example>
+        [DefaultValue(true)]
+        public bool ThrowErrorOnFailure { get; set; }
     }
 }
